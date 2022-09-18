f17d618fb07c13dd018de28b39e5be8831b72e55
inno-leokim <inno.leokim@gmail.com>
2022-09-19 01:06:32 +0900

    Add getEthBalance, createAccount

--- src/main/java/com/keymamo/wallet/controller/WalletController.java
+++ src/main/java/com/keymamo/wallet/controller/WalletController.java
@@ -5,10 +5,17 @@ import com.keymamo.wallet.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.crypto.CipherException;
 import org.web3j.protocol.core.methods.response.EthBlockNumber;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -21,11 +28,45 @@ public class WalletController {
         this.walletService = walletService;
     }
 
+    /**
+     *
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     @GetMapping("/api/v1/block-number")
     public BlockNumberDto getBlockNumber() throws ExecutionException, InterruptedException {
 
         BlockNumberDto blockNumberDto = new BlockNumberDto(walletService.getBlockNumber());
 
         return blockNumberDto;
+    }
+
+    /**
+     * 함수명 : createNewAccount
+     * 내용 : 계정 생성 함수
+     * @return
+     * @throws InvalidAlgorithmParameterException
+     * @throws CipherException
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     * @throws NoSuchProviderException
+     */
+    @PostMapping("/api/v1/wallet")
+    public String createNewAccount() throws InvalidAlgorithmParameterException, CipherException, NoSuchAlgorithmException, IOException, NoSuchProviderException {
+        return walletService.createAccount();
+    }
+
+    /**
+     * 함수명 : getEtherBalance
+     * 내용 : 특정 계정의 이더리움 잔액체크
+     * @param address
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @GetMapping("/api/v1/balance/{address}")
+    public BigInteger getEtherBalance(@PathVariable(value = "address", required = true) String address) throws ExecutionException, InterruptedException {
+        return walletService.getEtherBalance(address);
     }
 }
--- src/main/java/com/keymamo/wallet/service/WalletService.java
+++ src/main/java/com/keymamo/wallet/service/WalletService.java
@@ -2,29 +2,55 @@ package com.keymamo.wallet.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class WalletService {
 
     private final Web3j web3j;
-    private EthBlockNumber blockNumber;
 
     @Autowired
     public WalletService(Web3j web3j) {
         this.web3j = web3j;
-        this.blockNumber = new EthBlockNumber();
     }
 
     public BigInteger getBlockNumber() throws ExecutionException, InterruptedException {
+
+        EthBlockNumber blockNumber = new EthBlockNumber();
+
         blockNumber = web3j.ethBlockNumber()
                 .sendAsync()
                 .get();
 
         return blockNumber.getBlockNumber();
+    }
+
+    public String createAccount() throws InvalidAlgorithmParameterException, CipherException, NoSuchAlgorithmException, IOException, NoSuchProviderException {
+        return WalletUtils.generateNewWalletFile(
+                "1234",
+                new File("/Users/brand13/Dev/java-study/wallet/accounts-files")
+        );
+    }
+
+    public BigInteger getEtherBalance(String address) throws ExecutionException, InterruptedException {
+        EthGetBalance balance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST)
+                                    .sendAsync()
+                                    .get();
+
+        return balance.getBalance();
     }
 }
